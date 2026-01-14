f1c8e7485aa1c53dec0a4bd8a16b8494c10d11bf
siddhant <siddhantj802@.com>
2026-01-14 19:47:56 +0530

    converting to RGB and comparing values

--- script.py
+++ script.py
@@ -1,20 +1,23 @@
 from PIL import Image
-import os 
-
-size = (128, 128)
+import os
 input_path = "./input_images/sellerimg.png"
-output_dir = "./saved_images"
-output_path = os.path.join(output_dir , "sellerimg_resized.png")
+output_dir = './saved_images'
+output_path = os.path.join(output_dir, "sellerimg_resize.png")
+size = (20 , 20)
+
 try:
-     with Image.open(input_path) as img:
-        print(f"Original Image size is {img.size}")
-        img.thumbnail(size)
-        print(f"New Image size is {img.size}")
-        img.save(output_path)
-        print("Image saved successfully")
+    with Image.open(input_path) as img:
+        resized_img = img.resize(size)
+        rgb_img = resized_img.convert('RGB')
+        for x in range(20):
+            for y in range(20):
+                print(f"rgb for {x},{y} pixel is {rgb_img.getpixel((x,y))}")
 except FileNotFoundError:
-    print("Image not Found")
+    print("File Not FOund")
 except Exception as e:
     print("something went wrong",e)
 finally:
-    print("Operation Complete")
+    rgb_img.save(output_path)
+    rgb_img.show()
+    print('Operation Complete')
+
